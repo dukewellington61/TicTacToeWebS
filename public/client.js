b1df33015214ccd58fb60f0b740106805c0170a2
theIronDuke <dukewellington61@googlemail.com>
2019-10-28 20:28:20 +0100

    more layout stuff7

--- public/client.js
+++ public/client.js
@@ -423,13 +423,13 @@ const checkMobileScreenOrientation = () => {
 
   console.log('screen.orientation.angle: ' + screen.orientation.angle);
 
-  const mobilePortrait = screen.height > screen.width && screen.orientation.angle === 0;
+  const mobilePortrait = screen.height > screen.width && screen.orientation.angle === 0 || 180;
 
-  const mobileLandscape = screen.height < screen.width && screen.orientation.angle === 90;
+  const mobileLandscape = screen.height < screen.width && screen.orientation.angle === 90 || 270;
 
-  const tabletPortrait = screen.height > screen.width && screen.orientation.angle === 90;
+  const tabletPortrait = screen.height > screen.width && screen.orientation.angle === 90 || 270;
 
-  const tabletLandscape = screen.height < screen.width && screen.orientation.angle === 0;
+  const tabletLandscape = screen.height < screen.width && screen.orientation.angle === 0 || 180;
 
   if (mobilePortrait || tabletPortrait) {    
     document.querySelector('body').classList.add('screen-in-portrait-mode');

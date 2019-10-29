1afa4781b83246663e2d7ce490b8f993cf04395b
theIronDuke <dukewellington61@googlemail.com>
2019-10-29 23:10:35 +0100

    send app height to parent if mobile screen is turned in  portrait mode3

--- public/client.js
+++ public/client.js
@@ -454,7 +454,7 @@ const sendAppHeightToParentIfMobileScreenPortraitMode = () => {
 
   const mobilePortrait = screen.height > screen.width && screen.orientation.angle === 0;  
 
-  if (mobilePortrait) sendHeightToParentWindow();
+  if (mobilePortrait) console.log('mobilePortrait');
 };
   
 

ca7772c831d5d44361f8e30a57e89da1e0641a0a
theIronDuke <dukewellington61@googlemail.com>
2019-11-03 23:22:21 +0100

    tablet in portrait mode6

--- public/client.js
+++ public/client.js
@@ -339,6 +339,7 @@ window.addEventListener('message', msg => {
 socket.on('push', () => {
   viewTikTakToe();
   setTimeout( () => createPlayerNameInputField(), 200);
+  sendHeightToParentWindow();
 });
 
 socket.on('hide-start-button', () => hideStartButton());
